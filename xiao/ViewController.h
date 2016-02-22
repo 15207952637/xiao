451ccedfbf88a99fafb23c1aa53c1acdb75df390
Apple <apple@appledeimac.local>
2016-02-22 16:38:19 +0800

    Initial Commit

---
+++ xiao/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xiao
+//
+//  Created by Apple on 16/2/22.
+//  Copyright © 2016年 Ofiel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ xiao/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  xiao
+//
+//  Created by Apple on 16/2/22.
+//  Copyright © 2016年 Ofiel. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
